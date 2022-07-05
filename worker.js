59322f9cf4b912331e0e3a8121a8f3107cd53936
Jacob Gantt <gantt42@gmail.com>
2022-07-05 15:19:43 -0400

    Octal

--- worker.js
+++ worker.js
@@ -23,7 +23,7 @@ log4js.configure({
         },
         publish: {
             type: "file", filename: `${APP_NAME}-worker.log`,
-            mode: parseInt((666).toString(10), 8),
+            mode: 0o666,
             layout: {
                 type: "pattern",
                 pattern: "%d{yyyy/MM/dd-hh.mm.ss} [work] %p %c %m",
@@ -76,8 +76,8 @@ let initSite = async () => {
     let uuid = randomUUID();
     siteDir = `websites/${WEBSITE_NAME}-${uuid}/`;
     logDir = `website-logs/${WEBSITE_NAME}-${uuid}/`;
-    await fs.ensureDir(siteDir, parseInt((666).toString(10), 8));
-    await fs.ensureDir(logDir, parseInt((666).toString(10), 8));
+    await fs.ensureDir(siteDir, 0o666);
+    await fs.ensureDir(logDir, 0o666);
     logger.debug(`Node Site #${process.pid} starting.`);
     try {
         await fs.copy(`node_modules/${WEBSITE_NAME}/`, siteDir);
