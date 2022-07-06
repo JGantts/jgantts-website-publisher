3a659d0eaafeb4c28d07b1a979ffb1ec3301d714
Jacob Gantt <gantt42@gmail.com>
2022-07-06 15:50:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ let startWorkerPromise = () => {
     return new Promise(async (resolve, reject) => {
         logger.debug(`Forking worker.`);
         console.log("about to fork");
-        let newWorker = {};// fork(path.join(path.dirname(await fs.realpath(__filename)), 'worker.js'));
+        let newWorker = fork(path.join(path.dirname(await fs.realpath(__filename)), 'worker.js'));
         console.log("forked");
         logger.debug(`Forked worker.`);
         newWorker.on('message', async (msg) => {
