77c3ae534932674ce937ebf438c0a1f9f5ae8df9
Jacob Gantt <gantt42@gmail.com>
2022-07-07 10:31:08 -0400

    Create setup_jgantts-website-publisher.sh

---
+++ setup_jgantts-website-publisher.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Setting up jgantts-website-publisher for CentOS 7"
+useradd jgantts-website-publisher
+curl –sL https://rpm.nodesource.com/setup_16.x | bash -
+yum -y install git
+yum -y install nodejs
+cd /root
+git clone https://github.com/JGantts/jgantts-website-publisher.git
+cd jgantts-website-publisher
+npm install
+echo "cd /root/jgantts-website-publisher/ && npm start" >> /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
+systemctl enable rc-local
+echo "done"
+echo "Next steps for admin to perform:"
+echo -e "\t- ADD SSL Certs to /keys/jgantts.com/"
+echo -e "\t- ADD config file to /root/jgantts-website-publisher/"
+echo -e "\t- Reboot"
