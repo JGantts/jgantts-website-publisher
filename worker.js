ad95871c71ea1856c39afae6ea9b287c41e2d5af
Jacob Gantt <gantt42@gmail.com>
2022-07-06 23:26:12 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -84,7 +84,7 @@ let initSite = async () => {
         logger.debug(`cwd: ${process.cwd()}`);
         await install(siteDir);
 
-        logger.debug(fs.existsSync(siteDir + `out/index.js`));
+        logger.debug(fs.existsSync(siteDir + `/package.js`));
         site = require(siteDir + `out/index.js`);
         process.on('message', receivedMessage);
         let tempWorkindDir = process.cwd();
