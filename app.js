e604445e498362d2ea4e23f47a6f75f7e29f0baf
Jacob Gantt <gantt42@gmail.com>
2022-07-07 03:33:37 -0400

    Whitespace

--- app.js
+++ app.js
@@ -157,213 +157,239 @@ let changeOwnerToLeastPrivilegedUser = async (path) => {
                     resolve();
                 }
             });
-        });
-    }
-
-    let startWorkers = async () => {
-        logger.debug("startWorkers")
-        for (let i = 0; i < WORKER_TOTAL; i++) {
-            await startWorker();
         }
-        logger.debug("done startWorkers")
-    }
+    );
+}
 
-    let restartWorkers = async () => {
-        logger.debug("restartWorkers")
-        Object.keys(workerBodies).forEach(async (workerKey) => {
-            await restartWorker(workerBodies[workerKey]);
-        });
-        logger.debug("done restartWorkers")
+let startWorkers = async () => {
+    logger.debug("startWorkers")
+    for (let i = 0; i < WORKER_TOTAL; i++) {
+        await startWorker();
     }
+    logger.debug("done startWorkers")
+}
 
-    let restartWorker = (oldWorkerBody) => {
-        return new Promise(async (resolve, reject) => {
-            if (oldWorkerBody !== null) {
-                await killWorker(oldWorkerBody);
-            }
-            await startWorker();
+let restartWorkers = async () => {
+    logger.debug("restartWorkers")
+    Object.keys(workerBodies).forEach(async (workerKey) => {
+        await restartWorker(workerBodies[workerKey]);
+    });
+    logger.debug("done restartWorkers")
+}
+
+let restartWorker = (oldWorkerBody) => {
+    return new Promise(async (resolve, reject) => {
+        if (oldWorkerBody !== null) {
+            await killWorker(oldWorkerBody);
+        }
+        await startWorker();
+        resolve();
+    });
+}
+
+let killWorker = (workerBody) => {
+    logger.debug(new Error().stack);
+    return new Promise(async (resolve, reject) => {
+        workerBody.active = false;
+        workerBody.shutdownCallback = () => {
+            delete workerBodies[workerBody.uuid];
             resolve();
-        });
-    }
-
-    let killWorker = (workerBody) => {
-        logger.debug(new Error().stack);
-        return new Promise(async (resolve, reject) => {
-            workerBody.active = false;
-            workerBody.shutdownCallback = () => {
-                delete workerBodies[workerBody.uuid];
-                resolve();
-            };
-            workerBody.worker.send({type: 'shutdown'});
-        });
-    }
-
-    let getPortPromise = (worker) => {
-        return new Promise(async (resolve, reject) => {
-            logger.debug('Getting port');
-            worker.on('message', (msg) => {
-                switch (msg.type){
-                    case "port":
-                    if (msg.content.success) {
-                        logger.debug(`Listening on port ${msg.content.port}`);
-                        resolve(msg.content.port);
-                    } else {
-                        reject();
-                    }
-                    break;
-
-                    default:
-                    reject();
-                    break;
-                }
-            });
-            worker.send({type: 'port'});
-        });
-    }
-
-    let startWorker = async () => {
-        let newWorker = await startWorkerPromise();
-        logger.debug(`Worker ${newWorker.pid} fork.`);
-        let port = await getPortPromise(newWorker);
-        let workerBody = {
-            uuid: randomUUID(),
-            worker: newWorker,
-            active: true,
-            port: port,
-            activeConnections: 0,
-            heartbeat: false,
-            shutdownCallback: () => {}
         };
+        workerBody.worker.send({type: 'shutdown'});
+    });
+}
+
+let getPortPromise = (worker) => {
+    return new Promise(async (resolve, reject) => {
+        logger.debug('Getting port');
+        worker.on('message', (msg) => {
+            switch (msg.type){
+                case "port":
+                if (msg.content.success) {
+                    logger.debug(`Listening on port ${msg.content.port}`);
+                    resolve(msg.content.port);
+                } else {
+                    reject();
+                }
+                break;
+
+                default:
+                reject();
+                break;
+            }
+        });
+        worker.send({type: 'port'});
+    });
+}
+
+let startWorker = async () => {
+    let newWorker = await startWorkerPromise();
+    logger.debug(`Worker ${newWorker.pid} fork.`);
+    let port = await getPortPromise(newWorker);
+    let workerBody = {
+        uuid: randomUUID(),
+        worker: newWorker,
+        active: true,
+        port: port,
+        activeConnections: 0,
+        heartbeat: false,
+        shutdownCallback: () => {}
+    };
+    newWorker.on('message', async (msg) => {
+        logger.debug(`${msg.type}`);
+        switch (msg.type){
+            case "heartbeat":
+            logger.debug(`heartbeat = ${msg.content.heartbeat}`)
+            workerBody.heartbeat = msg.content;
+            break;
+
+            case "shutdown":
+            workerBody.worker.kill();
+            workerBody.shutdownCallback();
+            break;
+        }
+    });
+    newWorker.on('Error', (err) => {
+        logger.debug(err.message);
+    });
+    workerBodies[workerBody.uuid] = workerBody;
+}
+
+let startWorkerPromise = () => {
+    return new Promise(async (resolve, reject) => {
+        logger.debug(`Forking worker.`);
+        let newWorker = fork(path.join(path.dirname(await fs.realpath(__filename)), 'worker.js'));
+        logger.debug(`Forked worker.`);
         newWorker.on('message', async (msg) => {
             logger.debug(`${msg.type}`);
             switch (msg.type){
-                case "heartbeat":
-                logger.debug(`heartbeat = ${msg.content.heartbeat}`)
-                workerBody.heartbeat = msg.content;
+                case "start":
+                resolve(newWorker);
                 break;
 
-                case "shutdown":
-                workerBody.worker.kill();
-                workerBody.shutdownCallback();
+                default:
+                reject();
                 break;
             }
         });
-        newWorker.on('Error', (err) => {
-            logger.debug(err.message);
-        });
-        workerBodies[workerBody.uuid] = workerBody;
-    }
+    });
+}
 
-    let startWorkerPromise = () => {
-        return new Promise(async (resolve, reject) => {
-            logger.debug(`Forking worker.`);
-            let newWorker = fork(path.join(path.dirname(await fs.realpath(__filename)), 'worker.js'));
-            logger.debug(`Forked worker.`);
-            newWorker.on('message', async (msg) => {
-                logger.debug(`${msg.type}`);
-                switch (msg.type){
-                    case "start":
-                    resolve(newWorker);
-                    break;
+let checkStatusandVersion = async () => {
+    logger.debug(`check version and status`);
+    await checkVersion();
+    await checkStatus();
+}
 
-                    default:
-                    reject();
-                    break;
-                }
-            });
-        });
-    }
-
-    let checkStatusandVersion = async () => {
-        logger.debug(`check version and status`);
-        await checkVersion();
-        await checkStatus();
-    }
-
-    let checkStatus = async () => {
-        Object.keys(workerBodies).forEach(async (workerKey) => {
-            let workerBody = workerBodies[workerKey]
-            if (workerBody.worker.exitCode !== null) {
+let checkStatus = async () => {
+    Object.keys(workerBodies).forEach(async (workerKey) => {
+        let workerBody = workerBodies[workerKey]
+        if (workerBody.worker.exitCode !== null) {
+            await restartWorker(workerBody);
+        } else {
+            workerBody.heartbeat = false;
+            workerBody.worker.send({type: "heartbeat"});
+            await delay(1000);
+            if (!workerBody.heartbeat) {
                 await restartWorker(workerBody);
+            }
+        }
+    });
+}
+
+let delay = async (ms) => {
+    return new Promise((resolve, reject) => setTimeout(resolve, ms));
+}
+
+let checkVersion = async () => {
+
+    const packageRegistry = `https://registry.npmjs.org/${WEBSITE_NAME}`;
+
+    getJsonFromUri(packageRegistry, async (res) => {
+        if (res.error) { console.error(res.message); return; }
+        else if (!res.continue) { console.error(res.message); return; }
+
+        const packageMatadata = res.data
+        let versionsMetadata = packageMatadata.versions
+        let highestVersion = null;
+        for(var version in versionsMetadata) {
+            if(highestVersion === null) {
+                highestVersion = version;
             } else {
-                workerBody.heartbeat = false;
-                workerBody.worker.send({type: "heartbeat"});
-                await delay(1000);
-                if (!workerBody.heartbeat) {
-                    await restartWorker(workerBody);
-                }
-            }
-        });
-    }
-
-    let delay = async (ms) => {
-        return new Promise((resolve, reject) => setTimeout(resolve, ms));
-    }
-
-    let checkVersion = async () => {
-
-        const packageRegistry = `https://registry.npmjs.org/${WEBSITE_NAME}`;
-
-        getJsonFromUri(packageRegistry, async (res) => {
-            if (res.error) { console.error(res.message); return; }
-            else if (!res.continue) { console.error(res.message); return; }
-
-            const packageMatadata = res.data
-            let versionsMetadata = packageMatadata.versions
-            let highestVersion = null;
-            for(var version in versionsMetadata) {
-                if(highestVersion === null) {
+                if (compareVersions(highestVersion, version) <= 0) {
                     highestVersion = version;
-                } else {
-                    if (compareVersions(highestVersion, version) <= 0) {
-                        highestVersion = version;
-                    }
                 }
             }
+        }
 
-            let packageFile = `node_modules/${WEBSITE_NAME}/package.json`;
+        let packageFile = `node_modules/${WEBSITE_NAME}/package.json`;
 
-            if (fsSync.existsSync(packageFile)) {
-                const packageFileSting = (await fs.readFile(packageFile)).toString();
-                const packageFileJson = JSON.parse(packageFileSting);
-                const installedVersion = packageFileJson.version;
-                if (compareVersions(installedVersion, highestVersion) >= 0) {
-                    logger.debug(`${WEBSITE_NAME} is already up-to-date @${highestVersion}.`);
-                    return;
-                }
+        if (fsSync.existsSync(packageFile)) {
+            const packageFileSting = (await fs.readFile(packageFile)).toString();
+            const packageFileJson = JSON.parse(packageFileSting);
+            const installedVersion = packageFileJson.version;
+            if (compareVersions(installedVersion, highestVersion) >= 0) {
+                logger.debug(`${WEBSITE_NAME} is already up-to-date @${highestVersion}.`);
+                return;
             }
+        }
 
-            logger.debug(`Updating ${WEBSITE_NAME} module`);
-            exec(`npm install ${WEBSITE_NAME}@${highestVersion}`, async function(error, stdout, stderr){
-                logger.debug(`Done updating ${WEBSITE_NAME} module`);
-                logger.debug(stdout);
-                logger.debug(stderr);
-                restartWorkers();
-            });
+        logger.debug(`Updating ${WEBSITE_NAME} module`);
+        exec(`npm install ${WEBSITE_NAME}@${highestVersion}`, async function(error, stdout, stderr){
+            logger.debug(`Done updating ${WEBSITE_NAME} module`);
+            logger.debug(stdout);
+            logger.debug(stderr);
+            restartWorkers();
         });
-    }
+    });
+}
 
-    let getJsonFromUri = async (uri, then) => {
-        https.get(uri, (res) => {
-            logger.debug("response");
-            const { statusCode } = res;
-            const contentType = res.headers['content-type'];
+let getJsonFromUri = async (uri, then) => {
+    https.get(uri, (res) => {
+        logger.debug("response");
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
 
-            let err;
-            // Any 2xx status code signals a successful response but
-            // here we're only checking for 200.
-            if (statusCode !== 200) {
-                err = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-            } else if (!/^application\/json/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                `Expected application/json but received ${contentType}`);
-            }
-            if (err) {
+        let err;
+        // Any 2xx status code signals a successful response but
+        // here we're only checking for 200.
+        if (statusCode !== 200) {
+            err = new Error('Request Failed.\n' +
+            `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+            `Expected application/json but received ${contentType}`);
+        }
+        if (err) {
+            logger.debug(err);
+            logger.debug(err.message);
+            // Consume response data to free up memory
+            res.resume();
+            then({
+                error: true,
+                continue: false,
+                message: `${err.message}`,
+                data: null
+            });
+            return;
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                then({
+                    error: false,
+                    continue: true,
+                    message: ``,
+                    data: parsedData
+                });
+                return;
+            } catch (err) {
                 logger.debug(err);
                 logger.debug(err.message);
-                // Consume response data to free up memory
-                res.resume();
                 then({
                     error: true,
                     continue: false,
@@ -372,43 +398,18 @@ let changeOwnerToLeastPrivilegedUser = async (path) => {
                 });
                 return;
             }
-
-            res.setEncoding('utf8');
-            let rawData = '';
-            res.on('data', (chunk) => { rawData += chunk; });
-            res.on('end', () => {
-                try {
-                    const parsedData = JSON.parse(rawData);
-                    then({
-                        error: false,
-                        continue: true,
-                        message: ``,
-                        data: parsedData
-                    });
-                    return;
-                } catch (err) {
-                    logger.debug(err);
-                    logger.debug(err.message);
-                    then({
-                        error: true,
-                        continue: false,
-                        message: `${err.message}`,
-                        data: null
-                    });
-                    return;
-                }
-            });
-        }).on('error', (err) => {
-            logger.debug(err);
-            logger.debug(`Got error: ${err.message}`);
-            then({
-                error: true,
-                continue: false,
-                message: `${err.message}`,
-                data: null
-            });
-            return;
         });
-    }
+    }).on('error', (err) => {
+        logger.debug(err);
+        logger.debug(`Got error: ${err.message}`);
+        then({
+            error: true,
+            continue: false,
+            message: `${err.message}`,
+            data: null
+        });
+        return;
+    });
+}
 
-    initilize();
+initilize();
