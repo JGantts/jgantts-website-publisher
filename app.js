e030a4ae14e92cbbd7e30ed425d1c2a582c0161c
Jacob Gantt <gantt42@gmail.com>
2022-07-06 20:03:06 -0400

    remove mode

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const WORKER_TOTAL = 4;
 let logger;
 
 let initilize = async () => {
-    await fs.ensureDir(config.security.workingDir, { mode: 0o666 });
+    await fs.ensureDir(config.security.workingDir);
     process.chdir(config.security.workingDir);
 
     log4js.configure({
