6f0e3d3d2286bf3b8f0548b1ac142e3f776ed888
Jacob Gantt <gantt42@gmail.com>
2022-07-09 03:02:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -126,23 +126,11 @@ let initilize = async () => {
         (await fs.exists(installDir)) &&
         (await fsPromises.readdir(installDir)).length !== 0;
 
-    logger.debug(`goodInstallDir '${goodInstallDir}'`);
-    logger.debug(`true '${true}'`);
-    logger.debug(`await fs.exists(installDir) '${await fs.exists(installDir)}'`);
-    logger.debug(`(await fsPromises.readdir(installDir)).length !== 0 '${(await fsPromises.readdir(installDir)).length !== 0}'`);
-    logger.debug(`(await fsPromises.readdir(installDir)).length '${(await fsPromises.readdir(installDir)).length}'`);
-    logger.debug(`fsPromises.readdir(installDir).length '${fsPromises.readdir(installDir).length}'`);
-    logger.debug(`installDir '${installDir}'`);
-
-    fsPromises.readdir(installDir, () => {
-        logger.debug(`read dir`);
-    });
-
     if (goodInstallDir) {
         await startWorkers();
         await checkStatusandVersion();
     } else {
-        await fs.mkdir(installDir);
+        await fs.ensureDir(installDir);
         await checkVersion();
         await startWorkers();
     }
