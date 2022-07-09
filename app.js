8b9497ab7f4b1c8e1ea9a79ae08b380b26247718
Jacob Gantt <gantt42@gmail.com>
2022-07-09 03:27:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -139,7 +139,9 @@ let initilize = async () => {
 let loadBalancerHandler = async (req, res) => {
     let query = url.parse(req.url, true);
     logger.debug(`HTTPS hit: ${query.pathname}`);
-    logger.debug(`\t${req.ips}`);
+    logger.debug(`\t${req.ips.reduce(total, curr) => {
+        return `${total}${curr} `; 
+    }}`);
     let keys = Object.keys(workerBodies);
     if (keys.length > 0) {
         let keyIndex = Math.floor(Math.random() * keys.length);
